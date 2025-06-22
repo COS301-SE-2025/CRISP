c3e69e4e580f5668a6af800c922f5a0df0b8a434
DiaanBotes <u22598538@tuks.co.za>
2025-06-22 19:04:04 +0200

    Added threat feed
    Repository for ThreatFeed model operations

---
+++ core/repositories/threat_feed_repository.py
@@ -0,0 +1,60 @@
+from core.patterns.observer.threat_feed import ThreatFeed
+
+class ThreatFeedRepository:
+    """
+    Repository for ThreatFeed model operations.
+    """
+    
+    @staticmethod
+    def get_by_id(feed_id):
+        """
+        Get a threat feed by ID.
+        """
+        try:
+            return ThreatFeed.objects.get(id=feed_id)
+        except ThreatFeed.DoesNotExist:
+            return None
+    
+    @staticmethod
+    def get_all():
+        """
+        Get all threat feeds.
+        """
+        return ThreatFeed.objects.all()
+    
+    @staticmethod
+    def get_external_feeds():
+        """
+        Get all external threat feeds.
+        """
+        return ThreatFeed.objects.filter(
+            is_external=True,
+            taxii_server_url__isnull=False,
+            taxii_collection_id__isnull=False
+        )
+    
+    @staticmethod
+    def create(data):
+        """
+        Create a new threat feed.
+        """
+        return ThreatFeed.objects.create(**data)
+    
+    @staticmethod
+    def update(feed_id, data):
+        """
+        Update a threat feed.
+        """
+        feed = ThreatFeed.objects.get(id=feed_id)
+        for key, value in data.items():
+            setattr(feed, key, value)
+        feed.save()
+        return feed
+    
+    @staticmethod
+    def delete(feed_id):
+        """
+        Delete a threat feed.
+        """
+        feed = ThreatFeed.objects.get(id=feed_id)
+        feed.delete()
