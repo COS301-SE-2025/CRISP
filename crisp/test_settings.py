a4da72893ce1d4f4905094a350095859dbf09448
Githubber0101 <u22574982@tuks.co.za>
2025-06-26 14:51:44 +0200

    Updated main

--- crisp/test_settings.py
+++
@@ -1,9 +0,0 @@
-from .settings import *
-
-# Use SQLite for testing
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
-    }
-}
