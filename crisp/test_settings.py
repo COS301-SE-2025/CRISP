f51f0a692985452c1dacef3491a9d4c9482b416f
DiaanBotes <u22598538@tuks.co.za>
2025-07-04 15:26:21 +0200

    Added Main for testing

--- crisp/test_settings.py
+++ crisp/test_settings.py
@@ -1,9 +1,54 @@
 from .settings import *
 
+TEST_RUNNER = 'crisp.main_test_runner.CRISPTestOrchestrator'
+
 # Use SQLite for testing
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
+        'NAME': ':memory:',
     }
 }
+
+# Disable migrations
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+    
+    def __getitem__(self, item):
+        return None
+
+MIGRATION_MODULES = DisableMigrations()
+
+# Reduce logging verbosity
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'WARNING',
+            'handlers': ['console'],
+        },
+        'core': {
+            'level': 'WARNING',
+            'handlers': ['console'],
+        },
+    },
+}
+
+# Speed up password hashing
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# Disable debug toolbar
+DEBUG = False
+DEBUG_TOOLBAR = False
+
+# Disable email sending
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
