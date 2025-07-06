a69d6fdb1dbdf6a443a48e2d04acbff6db1e9175
Githubber0101 <u22574982@tuks.co.za>
2025-07-06 15:55:45 +0200

    testing CI/CD functionality

--- core/models/models_all.py
+++
@@ -1,9 +0,0 @@
-# CRISP - All Models Consolidated
-# Single file containing all models for ultra-clean structure
-
-# Import all models from subdirectories
-from .stix_object import *
-from .trust_models.models import *
-from .indicator import *
-from .institution import *
-from .ttp_data import *
--- core/urls/threat_intel.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from core.views.api.threat_feed_views import ThreatFeedViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'feeds', ThreatFeedViewSet, basename='threat-feed')
-
-urlpatterns = router.urls
