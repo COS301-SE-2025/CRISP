40a1f82b4b5f21f8fd652861ed4182a1f14d92d5
Githubber0101 <u22574982@tuks.co.za>
2025-09-29 00:51:24 +0200

    remove old capstone unified

--- Capstone-Unified/core/trust_management/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrustManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.trust_management'
-    verbose_name = 'CRISP Trust Management'
--- Capstone-Unified/core/user_management/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.user_management'
-    verbose_name = 'CRISP User Management'
